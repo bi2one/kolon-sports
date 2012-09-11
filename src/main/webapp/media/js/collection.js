11e69d9aac2d30b16a911305fd1abe36bf828769
mskim <thooy@naver.com>
2012-09-11 15:41:03 +0900

    d

--- src/main/webapp/media/js/collection.js
+++ src/main/webapp/media/js/collection.js
@@ -13,11 +13,6 @@ var handleMouseover = function(e) {
 			$("img#foot_image").attr({src:"/kolon/media/images/collection_footer_interview.png"});
 		else if(lastID=='fourth-button')
 			$("img#foot_image").attr({src:"/kolon/media/images/collection_footer_backstage.png"});
-<<<<<<< HEAD
-
-=======
-		
->>>>>>> a02ef54f13bb28752a8a27f7c89c1e42983e7bb1
 	} else {
 		$("img#foot_image").attr({src:"/kolon/media/images/collection_footer.png"});
 	}
